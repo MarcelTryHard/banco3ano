0741f12de9be309c83c5e08ca7a416baf52f44e7
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2025-09-01 10:45:00 -0300

    primeiro commit

---
+++ banco3ano.sql
@@ -0,0 +1,14 @@
+create database pet;
+
+use pet;
+
+create table pet( 
+nome varchar(255),
+idade int(11),
+raca varchar(255),
+porte varchar(255),
+sexo varchar(255),
+responsavel varchar(255),
+id int(11),
+perfil_id int(11)
+);
